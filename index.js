c4ae9ef4a377d0b0c2547d0c63056a8a2c60910c
Roshan Patel <rpatel@noon.com>
2019-03-26 17:42:29 +0400

    lib/jsonnet updated

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var libjsonnet = require('./lib/libjsonnet');
 var Jsonnet = function() {
   var jsonnet_make = libjsonnet.cwrap('jsonnet_make', 'number', []);
   this.vm = jsonnet_make();
-  this.jsonnet_cleanup_string = libjsonnet.cwrap('jsonnet_cleanup_string', 'number', ['number', 'number']);
   this.jsonnet_evaluate_snippet = libjsonnet.cwrap('jsonnet_evaluate_snippet', 'number', ['number', 'string', 'string', 'number']);
   this.jsonnet_destroy = libjsonnet.cwrap('jsonnet_destroy', 'number', ['number']);
 };
@@ -17,7 +16,6 @@ Jsonnet.prototype.eval = function(code) {
   var error = libjsonnet.getValue(error_ptr, 'i32*');
   libjsonnet._free(error_ptr);
   var result = libjsonnet.Pointer_stringify(output_ptr);
-  this.jsonnet_cleanup_string(this.vm, output_ptr);
   if (error) {
     throw new Error(result);
   }
